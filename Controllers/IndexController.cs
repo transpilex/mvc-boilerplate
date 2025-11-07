ff7bceedb49bd994382da43ceb6fa65c01fab701
Anant-Navadiya <anantnavadiya1530@gmail.com>
2025-11-07 09:57:13 +0530

    initial commit

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PROJECT_NAME.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROJECT_NAME.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
